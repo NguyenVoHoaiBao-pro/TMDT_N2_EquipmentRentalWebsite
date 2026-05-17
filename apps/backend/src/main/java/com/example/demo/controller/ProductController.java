9035a0c79b3a7654d709a13aacbbafd5f46c4514
Vo Tan Sang <22130233@st.hcmuaf.edu.vn>
2026-05-17 22:20:16 +0700

    feat: implement success script to generate data for users

--- apps/backend/src/main/java/com/example/demo/controller/ProductController.java
+++ apps/backend/src/main/java/com/example/demo/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @RestController
-@RequestMapping("products")
+@RequestMapping("/products")
 public class ProductController extends BaseController {
 
     private final ProductService productService;
