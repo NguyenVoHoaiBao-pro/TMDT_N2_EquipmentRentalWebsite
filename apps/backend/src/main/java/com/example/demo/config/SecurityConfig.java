a90c9de3f94a82ab8701bf067d8371d92c2ea413
Vo Tan Sang <22130233@st.hcmuaf.edu.vn>
2026-06-21 17:28:52 +0700

    fix: fix error backend container unhealthy docker

--- apps/backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ apps/backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
         "/v3/api-docs/**",
         "/swagger-resources/**",
         "/webjars/**",
+        "/actuator/**", // Contain actuator endpoints
         "/ws/**"
     };
 
